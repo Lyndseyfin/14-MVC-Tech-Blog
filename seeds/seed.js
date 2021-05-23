d8166e2a44f62ff65a4723065dddab98e51db488
Lyndsey Finamore <lyndseyfinamore@gmail.com>
2021-05-23 13:02:16 -0600

    everything working

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,27 +1,38 @@
 const sequelize = require('../config/connection');
 const { Comment, Post, User } = require('../models');
 
-const commentData = require('./userData.json');
-const postData = require('./blogData.json');
-const userData = require('./commentData.json');
+const commentData = require('./commentData.json');
+const postData = require('./postData.json');
+const userData = require('./userData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await Comment.bulkCreate(commentData, {
-    returning: true
-  });
 
-  await Post.bulkCreate(postData, {
-    returning: true
-  });
-
-  await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true
-  });
-
-  process.exit(0);
-};
-
-seedDatabase();
+  const seedDatabase = async () => {
+    await sequelize.sync({
+      force: true
+    });
+  
+    const users = await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
+  
+    for (const post of postData) {
+      await post.create({
+        ...post,
+        user_id: users[Math.floor(Math.random() * users.length)].id,
+      });
+    }
+  
+    const comments = await Comment.bulkCreate(commentData, {
+      individualHooks: true,
+      returning: true,
+    });
+  
+    process.exit(0);
+  };
+}
+  
+  seedDatabase();
