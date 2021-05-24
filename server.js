749b164891dde7a3f85f71ee79424bf5667eaf8c
Lyndsey Finamore <lyndseyfinamore@gmail.com>
2021-05-24 16:55:16 -0600

    controller

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const session = require('express-session');
 const exphndl = require('express-handlebars');
-const routes = require('./controllers');
+const routes = require('./controller');
 const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
