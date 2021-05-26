d55fe030ad3c57a190290cbff96e50c76f3d317e
Lyndsey Finamore <lyndseyfinamore@gmail.com>
2021-05-26 08:13:35 -0600

    jaws

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
+      host: 'tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
       dialect: 'mysql',
       port: 3306
     }
