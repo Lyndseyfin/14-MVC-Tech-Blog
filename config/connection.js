a588c1a9b472cda961f9c6cb26cea88ee45bb010
Lyndsey Finamore <lyndseyfinamore@gmail.com>
2021-05-26 18:47:38 -0600

    server not running, LA help

--- config/connection.js
+++ config/connection.js
@@ -10,10 +10,10 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-      // host: 'localhost',
+      // host: 'tvcpw8tpu4jvgnnq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      host: 'localhost',
       dialect: 'mysql',
-      port: 3306
+      // port: 3306
     }
   );
 }
