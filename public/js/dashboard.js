8b17975572c5def50d772522fb592a4ca3027027
Lyndsey Finamore <lyndseyfinamore@gmail.com>
2021-05-27 14:59:30 -0600

    dashboard issues

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,46 +1,46 @@
-const newFormHandler = async (event) => {
-    event.preventDefault();
+// const newFormHandler = async (event) => {
+//     event.preventDefault();
   
-    const title = document.querySelector('#post-title').value.trim();
-    const description = document.querySelector('#post-desc').value.trim();
+//     const title = document.querySelector('#post-title').value.trim();
+//     const description = document.querySelector('#post-desc').value.trim();
   
-    if (title && description) {
-      const response = await fetch(`/api/posts`, {
-        method: 'POST',
-        body: JSON.stringify({ title, description }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+//     if (title && description) {
+//       const response = await fetch(`/api/posts`, {
+//         method: 'POST',
+//         body: JSON.stringify({ title, description }),
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//       });
   
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert('Failed to create new post.');
-      }
-    }
-  };
+//       if (response.ok) {
+//         document.location.replace('/dashboard');
+//       } else {
+//         alert('Failed to create new post.');
+//       }
+//     }
+//   };
   
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
+//   const delButtonHandler = async (event) => {
+//     if (event.target.hasAttribute('data-id')) {
+//       const id = event.target.getAttribute('data-id');
   
-      const response = await fetch(`/api/posts/${id}`, {
-        method: 'DELETE',
-      });
+//       const response = await fetch(`/api/posts/${id}`, {
+//         method: 'DELETE',
+//       });
   
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert('Failed to delete posts');
-      }
-    }
-  };
+//       if (response.ok) {
+//         document.location.replace('/dashboard');
+//       } else {
+//         alert('Failed to delete posts');
+//       }
+//     }
+//   };
   
-  document
-    .querySelector('.new-post-form')
-    .addEventListener('submit', newFormHandler);
+//   document
+//     .querySelector('.new-post-form')
+//     .addEventListener('submit', newFormHandler);
   
-  document
-    .querySelector('.post-list')
-    .addEventListener('click', delButtonHandler);
+//   document
+//     .querySelector('.post-list')
+//     .addEventListener('click', delButtonHandler);
