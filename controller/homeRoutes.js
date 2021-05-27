1266ce6bf51fa01431fa79e3409ce2b29eaeb0cf
Lyndsey Finamore <lyndseyfinamore@gmail.com>
2021-05-27 08:26:28 -0600

    home

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -6,7 +6,7 @@ router.get("/", async (req, res) => {
   // retrieving all posts
   try {
     const postData = await Post.findAll({
-      include: [{ model: User, attributes: ["email"]},],
+      include: [{ model: User, attributes: ["username"]},],
       
     });
 
@@ -18,102 +18,179 @@ router.get("/", async (req, res) => {
     res.render("homepage", {
       posts,
       logged_in: req.session.logged_in,
-      userName: req.session.userName,
+      // userName: req.session.userName,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// login
-router.get("/login", (req, res) => {
-  // If a session exists, redirect the request to the homepage
-  if (req.session.logged_in) {  res.redirect("/");
-    return;
-  }
-  
-  console.log("redirecting from login route")
-  // else{
- res.render("login");
-  
-});
-
-//signup
-router.get("/signup", (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect("/");
-    return;
-  }
-
-  res.render("signup");
-});
-
-router.get("/dashboard", withAuth, async (req, res) => {
-  try {
-    // Get all posts and JOIN with user data
-    const postData = await Post.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ["email"],
-        },
-      ],
-      where: {
-        user_id: req.session.user_id,
-      },
-    });
-
-    // Serialize data so the template can read it
-    const posts = postData.map((post) => post.get({ plain: true }));
-    console.log(posts);
-
-    // Pass serialized data and session flag into template
-    res.render("dashboard", {
-      posts,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get("/newpost", (req, res) => {
-  if (!req.session.logged_in) {
-    res.redirect("/login");
-    return;
-  }
-
-  res.render("newpost");
-});
-
-router.get("/updatePost", (req, res) => {
-  if (!req.session.logged_in) {
-    res.redirect("/login");
-    return;
-  }
-
-  res.render("updatePost");
-});
-
-//get request for single post
-router.get("/:id", withAuth, async (req, res) => {
+router.get('/post/:id', async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
       include: [
+        {
+          model: User,
+          attributes: ['username'],
+        }, {
+          model: Comment,
+          include: [
+            User
+          ]
+        }
       ],
     });
 
-    const post = postData.get({ plain: true });
-    console.log(post);
+    const post = postData.get({
+      plain: true
+    });
 
-    res.render("post", {
+    res.render('post', {
       ...post,
-      logged_in: req.session.logged_in,
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: {
+        exclude: ['password']
+      },
+      include: [{
+        model: Post
+      }],
+    });
+
+    const user = userData.get({
+      plain: true
+    });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+
+  res.render('login');
+});
+
+/// added this signup
+router.get('/signup', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/dashboard');
+    return;
+  }
+  res.render('signup');
+});
 
 module.exports = router;
+
+// // login
+// router.get("/login", (req, res) => {
+//   // If a session exists, redirect the request to the homepage
+//   if (req.session.logged_in) {  res.redirect("/");
+//     return;
+//   }
+  
+//   console.log("redirecting from login route")
+//   // else{
+//  res.render("login");
+  
+// });
+
+// //signup
+// router.get("/signup", (req, res) => {
+//   if (req.session.logged_in) {
+//     res.redirect("/");
+//     return;
+//   }
+
+//   res.render("signup");
+// });
+
+// router.get("/dashboard", withAuth, async (req, res) => {
+//   try {
+//     // Get all posts and JOIN with user data
+//     const postData = await Post.findAll({
+//       include: [
+//         {
+//           model: User,
+//           attributes: ["email"],
+//         },
+//       ],
+//       where: {
+//         user_id: req.session.user_id,
+//       },
+//     });
+
+//     // Serialize data so the template can read it
+//     const posts = postData.map((post) => post.get({ plain: true }));
+//     console.log(posts);
+
+//     // Pass serialized data and session flag into template
+//     res.render("dashboard", {
+//       posts,
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// router.get("/newpost", (req, res) => {
+//   if (!req.session.logged_in) {
+//     res.redirect("/login");
+//     return;
+//   }
+
+//   res.render("newpost");
+// });
+
+// router.get("/updatePost", (req, res) => {
+//   if (!req.session.logged_in) {
+//     res.redirect("/login");
+//     return;
+//   }
+
+//   res.render("updatePost");
+// });
+
+// //get request for single post
+// router.get("/:id", withAuth, async (req, res) => {
+//   try {
+//     const postData = await Post.findByPk(req.params.id, {
+//       include: [
+//       ],
+//     });
+
+//     const post = postData.get({ plain: true });
+//     console.log(post);
+
+//     res.render("post", {
+//       ...post,
+//       logged_in: req.session.logged_in,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+
+// module.exports = router;
